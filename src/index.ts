421a119a88b6613fbf5c8a56d44fd9f799c4de90
Anuj Singh <anujZzztw@gmail.com>
2022-06-03 23:15:24 +0530

    done testing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { scheduleJob } from "node-schedule";
 import { GetData } from "./puppeteer";
 
-scheduleJob("* * * * *", function () {
-  console.log("running a task every minute");
+scheduleJob("0 */3 * * *", function () {
+  console.log("running a task every 3hr");
   GetData();
 });
