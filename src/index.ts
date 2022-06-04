1d3bc1f6d9dc1a6a93312a7ca0f646661fe78adf
Anuj Singh <anujZzztw@gmail.com>
2022-06-04 09:47:44 +0530

    change it to 1 hr

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { scheduleJob } from "node-schedule";
 import { GetData } from "./puppeteer";
 
-scheduleJob("0 */3 * * *", function () {
+scheduleJob("0 * * * *", function () {
   console.log("running a task every 3hr");
   GetData();
 });
